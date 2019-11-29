e7476b9b3ab92d3b5b87a49021689f561b548983
mpaseltiner <mjp3591@rit.edu>
2019-11-29 12:29:55 -0500

    proper minwidth and minheight

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 800,
-    minHeight: 600,
+    width: 1200,
+    height: 800,
+    minWidth: 1200,
+    minHeight: 800,
     webPreferences: {
       nodeIntegration: true
     }
